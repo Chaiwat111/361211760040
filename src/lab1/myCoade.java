aa4b80e08baf169146923e31e220b68e026628a7
Chaiwat <Mr.chaiwat.2540@email.com>
2018-06-26 11:36:11 +0700

    ็Hello World

---
+++ src/lab1/myCoade.java
@@ -0,0 +1,25 @@
+package lab1;
+
+public class myCoade {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World.");
+        System.out.println("นาย ไชยวํฒน์ ทองมี");
+        System.out.println("361211760040");
+        System.out.println("MIT211");
+        System.out.println("ระบบสารสนเทศ");
+
+
+
+
+    }  //main
+
+
+
+
+
+
+
+}//class
